635295f34923011f28905cd81eb5ff55b99fa6ae
Imran Hossen <freelancerimran81@gmail.com>
2022-05-25 00:07:40 +0600

    user get api done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -11,13 +12,27 @@ app.use(cors({
 app.use(express.json());
 
 
-// carToolsManufacturer
-// y5ruZcy8Gzzr3Qaa
+
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fs9am.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 async function run() {
@@ -76,9 +91,7 @@ async function run() {
 
 
 
-        // =====================================================================>>
         // =======================ReviewCollection Read/Get=====================>>
-        // =====================================================================>>
 
         app.get('/review', async (req, res) => {
             const services = await reviewCollection.find({}).toArray();
@@ -95,15 +108,19 @@ async function run() {
         })
 
 
-        // =====================================================================>>
         // =======================PRODUCT ORDER Read/Get=====================>>
-        // =====================================================================>>
-        app.get('/order', async (req, res) => {
+
+        app.get('/order', verifyJWT, async (req, res) => {
             const customer = req.query.customer;
-            console.log(customer);
-            const query = { customer: customer }
-            const services = await orderCollection.find(query).toArray();
-            res.send(services)
+            const decodedEmail = req.decoded.email;
+            if (customer === decodedEmail) {
+                const query = { customer: customer }
+                const services = await orderCollection.find(query).toArray();
+                return res.send(services)
+            } else {
+                return res.status(403).send({ message: 'Forbidden Access' });
+            }
+
         })
 
         // =========================== Order Create/Post==========================>>
@@ -115,9 +132,15 @@ async function run() {
             res.send(result)
         })
 
-        // =====================================================================>>
+        // =============================USER GET================================>>
+
+        app.get('/user', verifyJWT, async (req, res) => {
+            const user = await userCollection.find().toArray()
+            res.send(user)
+        })
+
         // =============================USER PUT================================>>
-        // =====================================================================>>
+
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
@@ -127,7 +150,8 @@ async function run() {
                 $set: user
             };
             const result = await userCollection.updateOne(filter, updateDoc, option);
-            res.send(result)
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30d' })
+            res.send({ result, token })
         })
 
 
